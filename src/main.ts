0da7d3ec52b623d6b55b6c3dffd97b35d7bf2a8c
Niko Dunixi [He/Him] <1402178+niko-dunixi@users.noreply.github.com>
2023-07-04 16:13:47 -0500

    tailwind and styling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import './style.css'
 
-const url = new URL(window.location.href);
-if (url.host.endsWith("paulfreaknbaker.com")) {
-  url.host = url.host.replaceAll("paulfreaknbaker.com", "nikodunixi.com");
-  window.location.href = url.toString();
-}
+// const url = new URL(window.location.href);
+// if (url.host.endsWith("paulfreaknbaker.com")) {
+//   url.host = url.host.replaceAll("paulfreaknbaker.com", "nikodunixi.com");
+//   window.location.href = url.toString();
+// }
+
+// const redirectButtonContainerDiv: HTMLElement | null = window.document.getElementById('redirect-button-container');
+// if (redirectButtonContainerDiv != null && redirectButtonContainerDiv instanceof HTMLDivElement) {
+//   setupCountdownButton(redirectButtonContainerDiv);
+// }
+
+setTimeout(() => {
+  window.location.href = "https://www.nikodunixi.com/";
+}, 1000 * 15);
